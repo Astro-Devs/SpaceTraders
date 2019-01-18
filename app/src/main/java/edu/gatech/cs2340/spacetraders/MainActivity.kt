a34872ad3ac9cd6f433c1ec94b13d3b0d90369ba
Joshua Lau <jlau37@gatech.edu>
2019-01-18 14:04:47 -0500

    Initial commit

---
+++ app/src/main/java/edu/gatech/cs2340/spacetraders/MainActivity.kt
@@ -0,0 +1,12 @@
+package edu.gatech.cs2340.spacetraders
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
