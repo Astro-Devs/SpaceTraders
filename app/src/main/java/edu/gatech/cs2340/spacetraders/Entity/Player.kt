3b684b2b809321389e464547c344a10d966849cb
pchuh <phillip.hong1483@gmail.com>
2019-02-15 15:37:07 -0500

    finished Player class

--- app/src/main/java/edu/gatech/cs2340/spacetraders/Entity/Player.kt
+++ app/src/main/java/edu/gatech/cs2340/spacetraders/Entity/Player.kt
@@ -1,10 +1,6 @@
-package edu.gatech.cs2340.spacetraders.Entity
+package edu.gatech.cs2340.spacetraders.entity
 
-class Player {
-
-    private String name;
-
-    private int credit;
+class Player(private val name : String, private var skillArr : IntArray, private var credits : Int ) {
 
 
 }
