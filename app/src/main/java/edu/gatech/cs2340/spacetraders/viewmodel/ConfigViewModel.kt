7d4f70ef0b4c64eda313c7d1c13e673caf629073
pchuh <phillip.hong1483@gmail.com>
2019-02-15 19:10:41 -0500

    added ConfigViewModel.kt

--- app/src/main/java/edu/gatech/cs2340/spacetraders/viewmodel/ConfigViewModel.kt
+++ app/src/main/java/edu/gatech/cs2340/spacetraders/viewmodel/ConfigViewModel.kt
@@ -1,2 +1,21 @@
 package edu.gatech.cs2340.spacetraders.viewmodel
 
+import edu.gatech.cs2340.spacetraders.entity.GameDifficulty
+import edu.gatech.cs2340.spacetraders.model.ModelFacade
+
+class ConfigViewModel() {
+
+    var modelFacade : ModelFacade = ModelFacade.getInstance()
+
+    fun onOk(name : String, pilotPts : Int, engineerPts : Int, traderPts : Int, fighterPts : Int,
+                     gameDiff : GameDifficulty) : Boolean {
+        if (name != null && name.length >= 1 && (pilotPts + engineerPts + traderPts + fighterPts) == 16) {
+            //call modelFacade functions to create game/player instance
+            //modelFacade.createGame(...)
+            //
+            return true
+        } else {
+            return false
+        }
+    }
+}
