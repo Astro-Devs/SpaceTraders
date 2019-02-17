50c46c0ff79eaf5b01d54ad62c8fe5fd25ac5f77
Soham Sonthi <sohamsonthi@gmail.com>
2019-02-17 11:44:44 -0500

    Added modelfacade
    Created modelfacade as a static class using the private constructor principle. Edited viewmodel to update for methods

--- app/src/main/java/edu/gatech/cs2340/spacetraders/model/ModelFacade.kt
+++ app/src/main/java/edu/gatech/cs2340/spacetraders/model/ModelFacade.kt
@@ -1,2 +1,22 @@
 package edu.gatech.cs2340.spacetraders.model
 
+import edu.gatech.cs2340.spacetraders.entity.Game
+import edu.gatech.cs2340.spacetraders.entity.GameDifficulty
+import edu.gatech.cs2340.spacetraders.entity.Player
+
+class ModelFacade private constructor() {
+    companion object {
+        var modelFac : ModelFacade = ModelFacade()
+        lateinit var newGame: Game
+
+        fun getInstance(): ModelFacade {
+            return modelFac
+        }
+
+    }
+
+    fun createGame(difficulty: GameDifficulty, player: Player): Game {
+        newGame = Game(difficulty, player)
+        return newGame
+    }
+}
--- app/src/main/java/edu/gatech/cs2340/spacetraders/viewmodel/ConfigViewModel.kt
+++ app/src/main/java/edu/gatech/cs2340/spacetraders/viewmodel/ConfigViewModel.kt
@@ -1,6 +1,8 @@
 package edu.gatech.cs2340.spacetraders.viewmodel
 
+import android.arch.lifecycle.AndroidViewModel
 import edu.gatech.cs2340.spacetraders.entity.GameDifficulty
+import edu.gatech.cs2340.spacetraders.entity.Player
 import edu.gatech.cs2340.spacetraders.model.ModelFacade
 
 class ConfigViewModel() {
@@ -10,9 +12,13 @@ class ConfigViewModel() {
     fun onOk(name : String, pilotPts : Int, engineerPts : Int, traderPts : Int, fighterPts : Int,
                      gameDiff : GameDifficulty) : Boolean {
         if (name != null && name.length >= 1 && (pilotPts + engineerPts + traderPts + fighterPts) == 16) {
+
             //call modelFacade functions to create game/player instance
-            //modelFacade.createGame(...)
+            var arrSkill : IntArray = intArrayOf(pilotPts, engineerPts, traderPts, fighterPts)
+            var player: Player = Player(name, arrSkill)
+            modelFacade.createGame(gameDiff, player)
             //
+
             return true
         } else {
             return false
