eff663c4fc82e2c5819c00c4a949b2b5057f9223
Joshua Lau <jlau37@gatech.edu>
2019-02-20 16:56:56 -0500

    Add KDoc to GameDifficulty

--- app/src/main/java/edu/gatech/cs2340/spacetraders/entity/GameDifficulty.kt
+++ app/src/main/java/edu/gatech/cs2340/spacetraders/entity/GameDifficulty.kt
@@ -1,5 +1,10 @@
 package edu.gatech.cs2340.spacetraders.entity
 
+/**
+ * Game difficulty enum that shows the different
+ * difficulty levels that players can choose
+ * to play the game in
+ */
 enum class GameDifficulty {
     BEGINNER,
     EASY,
