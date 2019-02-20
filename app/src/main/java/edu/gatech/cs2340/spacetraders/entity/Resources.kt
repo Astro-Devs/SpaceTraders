56d52ddd692fb0424e839dc9b02a9b34db50b05d
Joshua Lau <jlau37@gatech.edu>
2019-02-20 18:08:37 -0500

    Add tech levels and resources enums

---
+++ app/src/main/java/edu/gatech/cs2340/spacetraders/entity/Resources.kt
@@ -0,0 +1,21 @@
+package edu.gatech.cs2340.spacetraders.entity
+
+/**
+ * Enum that lists the different resources
+ * for the solar systems
+ */
+enum class Resources(val level: Int) {
+    NOSPECIALRESOURCES(0),
+    MINERALRICH(1),
+    MINERALPOOR(2),
+    DESERT(3),
+    LOTSOFWATER(4),
+    RICHSOIL(5),
+    POORSOIL(6),
+    RICHFAUNA(7),
+    LIFELESS(8),
+    WEIRDMUSHROOMS(9),
+    LOTSOFHERBS(10),
+    ARTISTIC(11),
+    WARLIKE(12)
+}
---
+++ app/src/main/java/edu/gatech/cs2340/spacetraders/entity/TechLevels.kt
@@ -0,0 +1,17 @@
+package edu.gatech.cs2340.spacetraders.entity
+
+/**
+ * Enum that lists the different tech levels
+ * for the solar systems
+ */
+enum class TechLevels(val level: Int) {
+    PREAGRICULTURE(0),
+    AGRICULTURE(1),
+    MEDIEVAL(2),
+    RENAISSANCE(3),
+    EARLYINDUSTRIAL(4),
+    INDUSTRIAL(5),
+    POST_INDUSTRIAL(6),
+    HITECH(7)
+
+}
