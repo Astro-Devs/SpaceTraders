e2313dd49d9256ec1d5b52c4398f1fa2f67801b4
Joshua Lau <jlau37@gatech.edu>
2019-02-20 16:56:06 -0500

    KDoc for Player class

--- app/src/main/java/edu/gatech/cs2340/spacetraders/entity/Player.kt
+++ app/src/main/java/edu/gatech/cs2340/spacetraders/entity/Player.kt
@@ -1,5 +1,12 @@
 package edu.gatech.cs2340.spacetraders.entity
 
+/**
+ * General Player class that simulates the user's attributes
+ * @param name the name of the player
+ * @param skillArr the integer array of the skill points
+ * @param credits the money the player has
+ * @param ship the player's spaceship to traverse the galaxy
+ */
 class Player constructor(val name : String, var skillArr : IntArray, var credits : Int = 1000,
                          var ship : Ship = Ship.GNAT ) {
     override fun toString(): String {
