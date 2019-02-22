2d94b7a1cd3316cf011adf3f18efc4e5cda3f217
Joshua Lau <jlau37@gatech.edu>
2019-02-22 14:14:54 -0500

    Add Coordinates class with Point inner class

---
+++ app/src/main/java/edu/gatech/cs2340/spacetraders/entity/Coordinates.kt
@@ -0,0 +1,15 @@
+package edu.gatech.cs2340.spacetraders.entity
+
+import kotlin.math.abs
+import kotlin.math.hypot
+
+class Coordinates {
+    inner class Point(pointA : Int, pointB : Int) {
+        val pointA = pointA
+        val pointB = pointB
+    }
+    
+    fun distance(p1 : Point, p2 : Point) : Double {
+        return hypot(abs(p2.pointA - p1.pointA) as Double, abs(p2.pointB - p1.pointB) as Double)
+    }
+}
