d403fed7c1df19c27a8a7413ecf303002abbf074
Joshua Lau <jlau37@gatech.edu>
2019-02-22 23:15:43 -0500

    Add entire list of potential planet names

--- app/src/main/java/edu/gatech/cs2340/spacetraders/entity/Universe.kt
+++ app/src/main/java/edu/gatech/cs2340/spacetraders/entity/Universe.kt
@@ -34,7 +34,36 @@ class Universe {
     }
 
     val map : HashMap<Coordinates, SolarSystem> = HashMap()
-    val names = arrayListOf<String>("Festen",		// A great Scandinavian movie
+    val names = arrayListOf<String>("Acamar",
+        "Adahn",		// The alternate personality for The Nameless One in "Planescape: Torment"
+        "Aldea",
+        "Andevian",
+        "Antedi",
+        "Balosnee",
+        "Baratas",
+        "Brax",			// One of the heroes in Master of Magic
+        "Bretel",		// This is a Dutch device for keeping your pants up.
+        "Calondia",
+        "Campor",
+        "Capelle",		// The city I lived in while programming this game
+        "Carzon",
+        "Castor",		// A Greek demi-god
+        "Cestus",
+        "Cheron",
+        "Courteney",	// After Courteney Cox…
+        "Daled",
+        "Damast",
+        "Davlos",
+        "Deneb",
+        "Deneva",
+        "Devidia",
+        "Draylon",
+        "Drema",
+        "Endor",
+        "Esmee",		// One of the witches in Pratchett's Discworld
+        "Exo",
+        "Ferris",		// Iron
+        "Festen",		// A great Scandinavian movie
         "Fourmi",		// An ant, in French
         "Frolix",		// A solar system in one of Philip K. Dick's novels
         "Gemulon",
@@ -66,7 +95,65 @@ class Universe {
         "Malcoria",
         "Melina",
         "Mentar",		// The Psilon home system in Master of Orion
-        "Merik")
+        "Merik",
+        "Mintaka",
+        "Montor",		// A city in Ultima III and Ultima VII part 2
+        "Mordan",
+        "Myrthe",		// The name of my daughter
+        "Nelvana",
+        "Nix",			// An interesting spelling of a word meaning "nothing" in Dutch
+        "Nyle",			// An interesting spelling of the great river
+        "Odet",
+        "Og",			// The last of the witches in Pratchett's Discworld
+        "Omega",		// The end of it all
+        "Omphalos",		// Greek for navel
+        "Orias",
+        "Othello",		// From Shakespeare
+        "Parade",		// This word means the same in Dutch and in English
+        "Penthara",
+        "Picard",		// The enigmatic captain from ST:TNG
+        "Pollux",		// Brother of Castor
+        "Quator",
+        "Rakhar",
+        "Ran",			// A film by Akira Kurosawa
+        "Regulas",
+        "Relva",
+        "Rhymus",
+        "Rochani",
+        "Rubicum",		// The river Ceasar crossed to get into Rome
+        "Rutia",
+        "Sarpeidon",
+        "Sefalla",
+        "Seltrice",
+        "Sigma",
+        "Sol",			// That's our own solar system
+        "Somari",
+        "Stakoron",
+        "Styris",
+        "Talani",
+        "Tamus",
+        "Tantalos",		// A king from a Greek tragedy
+        "Tanuga",
+        "Tarchannen",
+        "Terosa",
+        "Thera",		// A seldom encountered Dutch girl's name
+        "Titan",		// The largest moon of Jupiter
+        "Torin",		// A hero from Master of Magic
+        "Triacus",
+        "Turkana",
+        "Tyrus",
+        "Umberlee",		// A god from AD&D, which has a prominent role in Baldur's Gate
+        "Utopia",		// The ultimate goal
+        "Vadera",
+        "Vagra",
+        "Vandor",
+        "Ventax",
+        "Xenon",
+        "Xerxes",		// A Greek hero
+        "Yew",			// A city which is in almost all of the Ultima games
+        "Yojimbo",		// A film by Akira Kurosawa
+        "Zalkon",
+        "Zuul")			// From the first Ghostbusters movie)
 
     /**
      * Randomly select names, tech levels, resource levels, and coordinates,
